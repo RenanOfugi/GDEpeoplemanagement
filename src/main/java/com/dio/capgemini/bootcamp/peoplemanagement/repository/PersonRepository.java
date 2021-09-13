51081e0fb159a320713189b5718f129869f5894c
RenanOfugi <renan.ofugi@gmail.com>
2021-09-12 22:02:02 -0300

    feat: Criado o repository para a classe PersonGDE

--- src/main/java/com/dio/capgemini/bootcamp/peoplemanagement/repository/PersonRepository.java
+++ src/main/java/com/dio/capgemini/bootcamp/peoplemanagement/repository/PersonRepository.java
@@ -1,2 +1,7 @@
-package com.dio.capgemini.bootcamp.peoplemanagement.repository;public interface PersonRepository {
+package com.dio.capgemini.bootcamp.peoplemanagement.repository;
+
+import com.dio.capgemini.bootcamp.peoplemanagement.entity.PersonGDE;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<PersonGDE, Long> {
 }
