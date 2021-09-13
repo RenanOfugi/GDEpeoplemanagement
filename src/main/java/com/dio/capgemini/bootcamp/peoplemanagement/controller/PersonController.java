1050a7fc80553dcfa5965f29e52ea99c8f33a8c1
RenanOfugi <renan.ofugi@gmail.com>
2021-09-12 22:03:08 -0300

    refactor: Modificado o controller para aceitar POST requests

--- src/main/java/com/dio/capgemini/bootcamp/peoplemanagement/controller/PersonController.java
+++ src/main/java/com/dio/capgemini/bootcamp/peoplemanagement/controller/PersonController.java
@@ -1,2 +1,26 @@
-package com.dio.capgemini.bootcamp.peoplemanagement.controller;public class PersonController {
+package com.dio.capgemini.bootcamp.peoplemanagement.controller;
+
+import com.dio.capgemini.bootcamp.peoplemanagement.dto.response.MessageResponseDTO;
+import com.dio.capgemini.bootcamp.peoplemanagement.entity.PersonGDE;
+import com.dio.capgemini.bootcamp.peoplemanagement.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+    private final PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(@RequestBody PersonGDE person) {
+        PersonGDE personGDESave = personRepository.save(person);
+        return MessageResponseDTO.builder().message("GDE " + personGDESave.getId() + ": Criado com sucesso").build();
+    }
+
 }
