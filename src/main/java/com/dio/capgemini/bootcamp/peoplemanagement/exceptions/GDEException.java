743851f347c793e8e153d89cd9eea4637f93c003
RenanOfugi <renan.ofugi@gmail.com>
2021-09-16 00:38:55 -0300

    feat: criado classe de excessão para pessoas não listadas

--- src/main/java/com/dio/capgemini/bootcamp/peoplemanagement/exceptions/GDEException.java
+++ src/main/java/com/dio/capgemini/bootcamp/peoplemanagement/exceptions/GDEException.java
@@ -1,4 +1,11 @@
 package com.dio.capgemini.bootcamp.peoplemanagement.exceptions;
 
-public class GDEException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class GDEException extends RuntimeException {
+    public GDEException(String message) {
+        super(message);
+    }
 }
